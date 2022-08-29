690417dc5d8b81b6ef53696c5841cc25a3530e87
imahdimir <imahdimir@gmail.com>
2022-08-29 20:28:21 +0430

    v1

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-##
-
 """
+Makes a map between Codal Tickers and Firm Tickers
+  - Gets all Codal Tickers from d-all-Codal-Letters data repository
+  - keeps unique ones
+  - Gets all Firm Tickers from d-id2ft data repository
+  - Maps exact matches
+  - applies nfas to Codal Tickers, and Firm Tickers
+  - Maps not mathced Codal Tickers if they exactly match after nfas
+  - Gets all multi Basetickers which are maps to same Firm Ticker
+  - Maps exact matches to same firmticker
 
+  - overwrites d-targ data.prq by builded one
+  - commits and pushes to d-targ data repository as a prq file
     """
-
+##
 import pandas as pd
 from githubdata import GithubData
 from mirutil.df_utils import read_data_according_to_type as rdata
+from mirutil.string_funcs import normalize_fa_str_completely as nfas
+from mirutil import utils as mu
+from mirutil import df_utils as dfu
 
 
-allcod_rp_url = 'https://github.com/imahdimir/d-all-Codal-Letters'
-t2b_rp_url = 'https://github.com/imahdimir/d-Ticker-2-BaseTicker-map'
-c2b_rp_url = 'https://github.com/imahdimir/d-CodalTicker-2-BaseTicker-map'
-bt_rp_url = 'https://github.com/imahdimir/d-uniq-BaseTickers'
+allcodal_url = 'https://github.com/imahdimir/d-all-Codal-Letters'
+id2ft_url = 'https://github.com/imahdimir/d-TSETMC_ID-2-FirmTicker'
+mb2ft_url = 'https://github.com/imahdimir/d-multi-BaseTickers-2-same-FirmTicker'
 
-cur_rp_url = 'https://github.com/imahdimir/make-CodalTicker-2-BaseTicker-map'
+targ_url = 'https://github.com/imahdimir/d-CodalTicker-2-FirmTicker'
+cur_url = 'https://github.com/imahdimir/b-d-CodalTicker-2-FirmTicker'
 
-tran = 'TracingNo'
-codtic = 'CodalTicker'
-cname = 'CompanyName'
-ltrcod = 'LetterCode'
-title = 'Title'
-pjdt = 'PublishDateTime'
-isest = 'IsEstimate'
-tic = 'Ticker'
-tid = 'TSETMC_ID'
+ftic = 'FirmTicker'
+ctic = 'CodalTicker'
 btic = 'BaseTicker'
 
 def main() :
@@ -32,119 +37,122 @@ def main() :
 
   ##
 
-  rp_ac = GithubData(allcod_rp_url)
+  rp_ac = GithubData(allcodal_url)
   ##
   rp_ac.clone()
   ##
-  dafp = rp_ac.data_filepath
+  dafp = rp_ac.data_fp
   da = rdata(dafp)
   ##
-  da = da[[codtic]].drop_duplicates()
+  da = da[[ctic]].drop_duplicates()
   da = da.dropna()
   ##
-  t2b_rp = GithubData(t2b_rp_url)
-  t2b_rp.clone()
-  ##
-  dftp = t2b_rp.data_filepath
-  dft = mf.read_data_according_to_type(dftp)
 
+  rp_i2f = GithubData(id2ft_url)
   ##
-  da[btic] = da[codtic].map(dft[btic])
-
+  rp_i2f.clone()
   ##
-  msk = da[btic].isna()
+  dftp = rp_i2f.data_fp
+  dft = rdata(dftp)
+  ##
+  dft = dft[[ftic]].drop_duplicates()
+  ##
+  dft['i'] = dft[ftic]
+  dft = dft.set_index('i')
+  ##
+  da[ftic] = da[ctic].map(dft[ftic])
+  ##
+  msk = da[ftic].isna()
   len(msk[msk])
-
   ##
-  da['1'] = da[codtic].apply(mf.normalize_fa_str)
-
+  da['1'] = da[ctic].apply(nfas)
   ##
-  dft = dft.reset_index()
-
-  dft['1'] = dft[tic].apply(mf.normalize_fa_str)
-
+  dft['1'] = dft[ftic].apply(nfas)
   dft = dft.set_index('1')
+  ##
+  msk = da[ftic].isna()
+  da.loc[msk , ftic] = da.loc[msk , '1'].map(dft[ftic])
+  ##
+  msk = da[ftic].isna()
+  df1 = da[msk]
+  len(msk[msk])
+  ##
+  msk = ~ dft[ftic].isin(da[ftic])
+  df2 = dft[msk]
 
   ##
-  msk = da[btic].isna()
-  da.loc[msk , btic] = da.loc[msk , '1'].map(dft[btic])
+  da = da[[ctic , ftic]]
 
   ##
-  msk = da[btic].isna()
+  rp_mb2ft = GithubData(mb2ft_url)
+  rp_mb2ft.clone()
+  ##
+  dmp = rp_mb2ft.data_fp
+  dm = rdata(dmp)
+  ##
+  dm = dm[[btic , ftic]].drop_duplicates()
+  ##
+  dm = dm.set_index(btic)
+  ##
+  msk = da[ftic].isna()
+  da.loc[msk , ftic] = da.loc[msk , ctic].map(dm[ftic])
+  ##
+  msk = da[ftic].isna()
+  df1 = da[msk]
+  len(msk[msk])
+  ##
+  dm = dm.reset_index()
+  dm['1'] = dm[btic].apply(nfas)
+  dm = dm.set_index('1')
+  ##
+  da['1'] = da[ctic].apply(nfas)
+  ##
+  msk = da[ftic].isna()
+  da.loc[msk , ftic] = da.loc[msk , '1'].map(dm[ftic])
+  ##
+  msk = da[ftic].isna()
   df1 = da[msk]
   len(msk[msk])
 
   ##
-  bt_rp = GithubData(bt_rp_url)
-  bt_rp.clone()
-
-  ##
-  dfbp = bt_rp.data_filepath
-  dfb = mf.read_data_according_to_type(dfbp)
-
-  ##
-  dfb['i'] = dfb[btic]
-  dfb = dfb.set_index('i')
-
-  ##
-  msk = da[btic].isna()
-  da.loc[msk , btic] = da.loc[msk , codtic].map(dfb[btic])
-
-  ##
-  msk = da[btic].isna()
-  df1 = da[msk]
-  len(msk[msk])
-
-  ##
-  msk = da[btic].isna()
-  da.loc[msk , btic] = da.loc[msk , '1'].map(dfb[btic])
-
-  ##
-  msk = da[btic].isna()
-  df1 = da[msk]
-  len(msk[msk])
-
-  ##
-  dfb = dfb.reset_index(drop = True)
-  dfb['1'] = dfb[btic].apply(mf.normalize_fa_str)
-  dfb = dfb.set_index('1')
-
-  ##
-  msk = da[btic].isna()
-  da.loc[msk , btic] = da.loc[msk , '1'].map(dfb[btic])
-
-  ##
-  msk = da[btic].isna()
-  df1 = da[msk]
-  len(msk[msk])
-
-  ##
-  da = da[[codtic , btic]]
+  da = da[[ctic , ftic]]
   da = da.dropna()
-  da = da.set_index(codtic)
 
   ##
-  c2b_rp = GithubData(c2b_rp_url)
-  c2b_rp.clone()
+  rp_targ = GithubData(targ_url)
+  rp_targ.clone()
 
   ##
-  dfcp = c2b_rp.data_filepath
+  dtp = rp_targ.data_fp
+  dfu.save_as_prq_wo_index(da , dtp)
 
   ##
-  mf.save_as_prq_wo_index(da , dfcp)
+  tokp = '/Users/mahdi/Dropbox/tok.txt'
+  tok = mu.get_tok_if_accessible(tokp)
 
   ##
-  msg = 'v1'
-  msg += 'by: ' + cur_rp_url
+  msg = 'builded'
+  msg += ' by: ' + cur_url
 
-  c2b_rp.commit_push(msg)
+  rp_targ.commit_and_push(msg , user = rp_targ.user_name , token = tok)
 
   ##
+
 
   rp_ac.rmdir()
-  c2b_rp.rmdir()
-  t2b_rp.rmdir()
-  bt_rp.rmdir()
+  rp_i2f.rmdir()
+  rp_mb2ft.rmdir()
+  rp_targ.rmdir()
+
+  ##
+
+  ##
 
 ##
+
+
+if __name__ == '__main__' :
+  main()
+
+
 ##
